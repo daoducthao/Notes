ba482da81d05c950d31dab5a422cfae9ebf74124
DC-VIETIS\ThaoDD <thaodd@vietis.com.vn>
2021-12-22 14:42:14 +0700

    Add project files.

---
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public NotesPage()
+        {
+            InitializeComponent();
+
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+
+    }
+}
